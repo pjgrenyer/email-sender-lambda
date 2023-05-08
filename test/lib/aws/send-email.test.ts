059dc66ce68a3b7e617339078cfc94164d9fbe97
Paul Grenyer <paul.grenyer@gmail.com>
2023-05-08 12:28:34 +0100

    chore: part way

--- test/lib/aws/send-email.test.ts
+++ test/lib/aws/send-email.test.ts
@@ -2,29 +2,27 @@ import sendEmail from './../../../src/lib/aws/send-email';
 import nodemailer from 'nodemailer';
 const aws = require('@aws-sdk/client-ses');
 jest.mock('@aws-sdk/client-ses', () => {
-    class SES {
-
-    }
+    class SES {}
 
     return {
-        SES
-    }
+        SES,
+    };
 });
 
 const mockSendEmail = jest.fn();
 
 jest.mock('nodemailer', () => {
-    return { 
+    return {
         createTransport: (transporter: any) => {
-            return { sendEmail: async () => mockSendEmail }
-        }
-     }
+            return { sendEmail: async () => mockSendEmail };
+        },
+    };
 });
 
 console.log(nodemailer.createTransport());
 
-describe("Send email", () => {
-    it("should populate all fields", async () => {
-        await sendEmail([],[],[],"","","");
-    })
-})
+describe('Send email', () => {
+    it('should populate all fields', async () => {
+        await sendEmail([], [], [], '', '', '');
+    });
+});
