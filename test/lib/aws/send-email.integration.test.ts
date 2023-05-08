b59eeb948de3a6bfb2eaf1923bd188bbc190dd8f
Paul Grenyer <paul.grenyer@gmail.com>
2023-05-08 07:25:16 +0100

    feat: removed email addresses

--- test/lib/aws/send-email.integration.test.ts
+++ test/lib/aws/send-email.integration.test.ts
@@ -1,7 +1,11 @@
 import sendEmail from './../../../src/lib/aws/send-email';
+import dotenv from 'dotenv';
+dotenv.config({ path: '.test.env' });
+
+const { TO_EMAILS, CC_EMAILS, BCC_EMAILS, FROM_EMAIL } = process.env;
 
 describe('AWS Send Email Integration Tests', () => {
     it('should send an email', async () => {
-        await sendEmail(['paul.grenyer@gmail.com', 'paulgrenyer@hotmail.com', 'paul.grenyer@haven.com'], [], [], 'subject', 'body', 'paul.grenyer@gmail.com');
+        await sendEmail(TO_EMAILS?.split(',') ?? [], CC_EMAILS?.split(',') ?? [], BCC_EMAILS?.split(',') ?? [], 'subject', '<h1>body</h1>', FROM_EMAIL);
     });
 });
