aec8039338b7341c3818542578502cf30d2d7e09
Paul Grenyer <paul.grenyer@gmail.com>
2023-05-24 19:34:46 +0100

    chore: update iam

--- infrastructure/iam.tf
+++ infrastructure/iam.tf
@@ -2,20 +2,20 @@ resource "aws_iam_role" "lambda" {
   name = "lambda"
 
   assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": "sts:AssumeRole",
-      "Principal": {
-        "Service": "lambda.amazonaws.com"
-      },
-      "Effect": "Allow",
-      "Sid": ""
-    }
-  ]
-}
-EOF
+  {
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Action": "sts:AssumeRole",
+        "Principal": {
+          "Service": "lambda.amazonaws.com"
+        },
+        "Effect": "Allow",
+        "Sid": ""
+      }
+    ]
+  }
+  EOF
 }
 
 resource "aws_iam_role_policy" "lambda_role_sqs_policy" {
